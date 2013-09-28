d0f62236113c5d702d24b70ae41ad0c4170e357b
Raul Fraile <raulfraile@gmail.com>
2013-09-29 01:35:47 +0200

    Added Request and Response inspectors

---
+++ Inspector/Object/Symfony/Component/HttpFoundation/Request.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * Ladybug: Simple and Extensible PHP Dumper
+ *
+ * Object/DomDocument dumper
+ *
+ * (c) Raúl Fraile Beneyto <raulfraile@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ladybug\Plugin\Symfony2\Inspector\Object\Symfony\Component\HttpFoundation;
+
+use Ladybug\Inspector\AbstractInspector;
+use Ladybug\Inspector\InspectorInterface;
+use Ladybug\Inspector\InspectorDataWrapper;
+use Ladybug\Type;
+
+class Request extends AbstractInspector
+{
+    public function accept(InspectorDataWrapper $data)
+    {
+        return InspectorInterface::TYPE_CLASS == $data->getType() && 'Symfony\Component\HttpFoundation\Request' === $data->getId();
+    }
+
+    public function getData(InspectorDataWrapper $data)
+    {
+        /** @var $var Symfony\Component\HttpFoundation\Request */
+        $var = $data->getData();
+
+        /** @var $code Type\Extended\CodeType */
+        $code = $this->extendedTypeFactory->factory('code', $this->level);
+
+        $code->setLanguage('http');
+        $code->setContent($var->__toString());
+        $code->setTitle('HTTP Request');
+
+        return $code;
+    }
+
+}
